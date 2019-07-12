740c72272a50a2411a072b4cbb8d9ef9ac6d8b08
Hung Nguyen <hung_van_nguyen@nitori.jp>
2019-07-12 15:45:04 +0900

    Fix deploy url

--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
 $('#getToken').click(() => {
   let id = $('#email').val();
   let pw = $('#password').val();
-  fetch(`http://localhost:5000/auth?id=${id}&pass=${encodeURIComponent(pw)}`)
+  fetch(`https://facebook-access-token.herokuapp.com/auth?id=${id}&pass=${encodeURIComponent(pw)}`)
     .then(e => e.json())
     .then(e => {
       let res = e.loc || e.error;
