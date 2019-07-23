447945cd3d53723e60d14cbca76ef5412a01b1cb
Hung Nguyen <hungnguyen241295@gmail.com>
2019-07-23 10:06:24 +0900

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
 $('#getToken').click(() => {
   let id = $('#email').val();
   let pw = $('#password').val();
-  fetch(`https://facebook-access-token.herokuapp.com/auth?id=${id}&pass=${encodeURIComponent(pw)}`)
+  fetch(`/auth?id=${id}&pass=${encodeURIComponent(pw)}`)
     .then(e => e.json())
     .then(e => {
       let res = e.loc || e.error;
